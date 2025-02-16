d32ecc1f40ff633c5bfdc421f1c71d4c903c7961
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-02-15 21:40:16 -0500

    work on 2.15.

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.post_index,name="post_index"),
+    path("tag/<tag>/",views.post_tag,name="post_tag"),
+    path('<slug:slug>',views.post_detail,name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,3 +9,20 @@ def post_index(request):
         "posts":posts,
     }
     return render(request,"posts/index.html",context)
+
+def post_tag(request, tag):
+    posts = Post.objects.filter(
+        tags__name__contains=tag
+    ).order_by=("-created_on")
+    context = {
+        "tag":tag,
+        "posts":posts,
+    }
+    return render(request, "posts/tag.html",context)
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+        "post":post,
+    }
+    return render(request, "posts/detail.html",context)
