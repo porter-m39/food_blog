37963ea4a2303ffe74a6494cc48daffcde431c0b
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-03-24 19:26:03 -0400

    working on admin site, and getting import/export working

--- posts/admin.py
+++ posts/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from posts.models import Post, Tag
+from import_export.admin import ImportExportActionModelAdmin
 
 # Register your models here.
 
 
 class TagAdmin(admin.ModelAdmin):
-    pass
+    def __str__(self):
+        return self.name
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(ImportExportActionModelAdmin):
     prepopulated_fields = {"slug": ("title",)} #automatically generates slug as you write a post
+    list_display = ["title", "created_on", "last_modified"]
+    search_fields = ["title"]
 
 admin.site.register(Tag,TagAdmin)
 admin.site.register(Post,PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -11,8 +11,9 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=225)
     body = models.TextField(blank=True) #body optional, in case you just want a headline or picture
+    source = models.CharField(max_length=200, blank=True)
     slug = models.SlugField(null=False,unique=True)
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField("Tag", related_name="posts", blank=True) #tags are optional
     banner = models.ImageField(default='default.png',blank=True) #image not required, we have a fallback image. Images should be added to media directory
