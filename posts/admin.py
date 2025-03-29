fc584f9574e3f71cb77f6416f23d38029302e82c
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-03-29 08:54:05 -0400

    lots of work on admin and import export features

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from posts.models import Post, Tag
 from import_export.admin import ImportExportActionModelAdmin
-from django.urls import reverse
-from django.utils.html import format_html
+from import_export.widgets import ManyToManyWidget
+from import_export import fields, resources
+
+# packages that I'll need if I want the display_tags to include links.
+# from django.urls import reverse
+# from django.utils.html import format_html
 
 # Register your models here.
 
@@ -10,17 +14,28 @@ from django.utils.html import format_html
 class TagAdmin(admin.ModelAdmin):
     def __str__(self):
         return self.name
+    
+class PostResource(resources.ModelResource):
+    tags = fields.Field(
+        attribute = 'tags',
+        widget=ManyToManyWidget(Tag, field='name', separator=', ')
+    )
+
+    class Meta:
+        model = Post
+        import_id_fields = ()  # Empty tuple means don't use IDs
+        fields = ('title', 'slug', 'created_on', 'last_modified', 'tags', 'source', 'banner', 'body')
+
 
 class PostAdmin(ImportExportActionModelAdmin):
+
+    resource_class = PostResource  # This connects the resource to the admin
     prepopulated_fields = {"slug": ("title",)} #automatically generates slug as you write a post
     list_display = ["title", "created_on", "last_modified", "display_tags"]
     search_fields = ["title"]
 
     def display_tags(self, obj):
-        list = []
-        for tag in obj.tags.all():
-            list.append(tag)
-        return list
+        return ", ".join([tag.name for tag in obj.tags.all()])
     
 
     #Below - some scrap work for making a display_tags which has links... not entirely working right now, would need to add stuff to the tags admin to get different links for each tag.
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+# auto slug imports
+from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import uuid
+
+def generate_unique_slug(title):
+    slug = slugify(title)
+    unique_slug = slug
+    num = 1
+    while Post.objects.filter(slug=unique_slug).exists():
+        unique_slug = f'{slug}-{uuid.uuid4().hex[:4]}'  # or {num}
+        num += 1
+    return unique_slug
+
 # Create your models here.
 
 class Tag(models.Model):
@@ -18,5 +34,10 @@ class Post(models.Model):
     tags = models.ManyToManyField("Tag", related_name="posts", blank=True) #tags are optional
     banner = models.ImageField(default='default.png',blank=True) #image not required, we have a fallback image. Images should be added to media directory
 
-    def __str__(self):
+    def __str__(self):# this is an example of a method. no migrations needed when implementing.
         return self.title
+    
+@receiver(pre_save, sender=Post)
+def generate_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = generate_unique_slug(instance.title)
