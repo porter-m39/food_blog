e03c93c35f8768df8c51ff8e6fc0411c393f7f0c
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-04-03 18:52:51 -0400

    working on getting source links to work

--- posts/models.py
+++ posts/models.py
@@ -28,6 +28,7 @@ class Post(models.Model):
     title = models.CharField(max_length=225)
     body = models.TextField(blank=True) #body optional, in case you just want a headline or picture
     source = models.CharField(max_length=200, blank=True)
+    source_link = models.URLField(max_length=200, blank=True)
     slug = models.SlugField(null=False,unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
