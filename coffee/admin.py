19d3cc7e79201c453b6d217c51972e9889538f7a
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-07-31 21:35:34 -0400

    improving admin for Coffee app

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from coffee.models import Roaster, Country, CuppingNote, Coffee, Score, Critic # , RoastLevel, Acidity, (not registering roast level and acidity, for now)
+from coffee.models import Roaster, Country, CuppingNote, Coffee, Score, Critic, RoastLevel, Acidity
+from import_export.admin import ImportExportActionModelAdmin
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
+from import_export import fields, resources
+
 
 # Register your models here.
 
@@ -12,14 +16,49 @@ class CountryAdmin(admin.ModelAdmin):
 class CuppingNoteAdmin(admin.ModelAdmin):
     pass
 
-class CoffeeAdmin(admin.ModelAdmin):
-    pass
-
 class CriticAdmin(admin.ModelAdmin):
     pass
 
 class ScoreAdmin(admin.ModelAdmin):
     pass
+
+class CoffeeResource(resources.ModelResource):
+    roaster = fields.Field(
+        attribute='roaster',
+        widget = ForeignKeyWidget(Roaster, field = 'name')
+    )
+
+    origin = fields.Field(
+        attribute='origin',
+        widget=ManyToManyWidget(Country,field='name',separator=', ')
+    )
+
+    roast_level = fields.Field(
+        attribute='roast_level',
+        widget = ForeignKeyWidget(RoastLevel, field = 'name')
+    )
+
+    acidity = fields.Field(
+        attribute='acidity',
+        widget = ForeignKeyWidget(Acidity, field = 'name')
+    )
+
+    cupping_notes = fields.Field(
+        attribute='cupping_notes',
+        widget=ManyToManyWidget(CuppingNote,field='name',separator=', ')
+    )
+
+    class Meta:
+        model = Coffee
+        import_id_fields = () # Empty tuple means don't use IDs
+        fields = ('name','roaster','origin','roast_level','acidity','cupping_notes','created_on','last_modified','comments')
+
+
+
+class CoffeeAdmin(ImportExportActionModelAdmin):
+    resource_class = CoffeeResource
+    list_display = ["name", "roaster", "created_on"]
+    search_fields = ["name","roaster__name"]
 
 
 admin.site.register(Roaster,RoasterAdmin)
