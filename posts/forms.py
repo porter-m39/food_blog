5ee4cf9edd9aec78a8cfa36c6e05840411c30218
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-11-18 21:00:35 -0500

    comments look a bit better, but still bad.

--- posts/forms.py
+++ posts/forms.py
@@ -6,11 +6,11 @@ class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
         widget = forms.TextInput(
-            attrs={"class":"form-control","placeholder":"Your Name"}
+            attrs={"class":"form-control comment-author","placeholder":"Your Name",}
         ),
     )
     body = forms.CharField(
         widget = forms.Textarea(
-            attrs = {"class":"form-control","placeholder":"Leave a comment!"}
+            attrs = {"class":"form-control comment-body","placeholder":"Leave a comment!"}
         )
     )
