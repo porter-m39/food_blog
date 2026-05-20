761988b7b0b9698bd7737be1cbf2bd672ce3e51f
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2026-05-19 20:25:08 -0400

    created an AppPassword in gmail

--- food_blog/settings.py
+++ food_blog/settings.py
@@ -289,5 +289,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL')
-EMAIL_HOST_PASSWORD = env('ACCT_PASSWORD')  # Use an App Password, not your Gmail password
+EMAIL_HOST_PASSWORD = env('APP_PASSWORD')  # Use an App Password, not your Gmail password
 DEFAULT_FROM_EMAIL = env('EMAIL')
